a76567955e6bb2d6ce22a777b22b18050b8492ad
candinhojr <candinholuiz@gmail.com>
2020-06-02 00:25:06 -0300

    create the pages of authors and books

--- src/Autores.js
+++ src/Autores.js
@@ -1,14 +1,54 @@
-import React, { Fragment } from 'react';
+import React, { Component, Fragment } from 'react';
 import Header from './Header';
+import DataTable from './DataTable';
 
-const Autores = () => {
+class Autores extends Component {
+    constructor(props) {
+        super(props);
 
-    return (
-        <Fragment>
-            <Header />
-            <h1>Autores</h1>
-        </Fragment>
-    )
+        this.state = {
+            autores: [
+                {
+                    nome: 'Zygmunt Bauman',
+                    livro: 'Modernidade Líquida',
+                    preco: 'R$ 100'
+                },
+                {
+                    nome: 'Zygmunt Bauman',
+                    livro: 'Amor Líquido',
+                    preco: 'R$ 100'
+                },
+                {
+                    nome: 'Zygmunt Bauman',
+                    livro: 'Vida Líquida',
+                    preco: 'R$ 100'
+                },
+                {
+                    nome: 'Zygmunt Bauman',
+                    livro: 'Medo Líquido',
+                    preco: 'R$ 100'
+                },
+                {
+                    nome: 'Zygmunt Bauman',
+                    livro: 'Tempos Líquidos',
+                    preco: 'R$ 100'
+                }
+            ],
+            titulo: 'Autores'
+        };
+    }
+
+    render() {
+        return (
+            <Fragment>
+                <Header />
+                <div className='container'>
+                    <h1>Página de Autores</h1>
+                    <DataTable dados={this.state.autores} titulo={this.state.titulo} colunas={['nome']} />
+                </div>
+            </Fragment>
+        );
+    }
 }
 
 export default Autores;
--- src/Livros.js
+++ src/Livros.js
@@ -1,14 +1,59 @@
-import React, { Fragment } from 'react';
+import React, { Component, Fragment } from 'react';
 import Header from './Header';
 
-const Livros = () => {
+import DataTable from './DataTable'
 
-    return (
-        <Fragment>
-            <Header />
-            <h1>Livros</h1>
-        </Fragment>
-    )
+class Livros extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            autores: [
+                {
+                    nome: 'Zygmunt Bauman',
+                    livro: 'Modernidade Líquida',
+                    preco: 'R$ 100'
+                },
+                {
+                    nome: 'Zygmunt Bauman',
+                    livro: 'Amor Líquido',
+                    preco: 'R$ 100'
+                },
+                {
+                    nome: 'Zygmunt Bauman',
+                    livro: 'Vida Líquida',
+                    preco: 'R$ 100'
+                },
+                {
+                    nome: 'Zygmunt Bauman',
+                    livro: 'Medo Líquido',
+                    preco: 'R$ 100'
+                },
+                {
+                    nome: 'Zygmunt Bauman',
+                    livro: 'Tempos Líquidos',
+                    preco: 'R$ 100'
+                }
+            ],
+            titulo: 'Livros'
+        }
+    }
+
+
+
+    render() {
+        return (
+            <Fragment>
+                <Header />
+                <div className="container">
+                    <h1>Livros</h1>
+                    <DataTable dados={this.state.autores} titulo={this.state.titulo} colunas={['livro']} />
+
+                </div>
+            </Fragment>
+        );
+    }
 }
 
 export default Livros;
