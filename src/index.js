33570c501a0a06837df3e55275754011f6e4e657
candinhojr <candinholuiz@gmail.com>
2020-06-03 15:49:31 -0300

    use Material UI and refactor the Tabela

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import './index.css';
 import Home from './Pages/Home/Home';
-import * as serviceWorker from './serviceWorker';
 import Sobre from './Pages/Sobre/Sobre';
 import NotFound from './Pages/NotFound/NotFound';
 import Autores from './Pages/Autores/Autores';
@@ -21,8 +20,3 @@ ReactDOM.render(
   </BrowserRouter>,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
